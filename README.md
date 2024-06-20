def811f435e1455913d4735ecd1acf435cf993b6
DeF <DeF-2008@yandex.ru>
2024-06-20 13:30:08 +0300

    first

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/c9ctrivhtsp6uxfw/branch/main?svg=true)](https://ci.appveyor.com/project/8DeF8/patterns1/branch/main)
--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -1,11 +1,14 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
+
 
 import java.time.Duration;
 
@@ -16,8 +19,19 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
-    void setup() { open("http://localhost:9999/"); }
+    void setup() {
+        open("http://localhost:9999/"); }
 
     @Test
     @DisplayName("Should successful plan meeting")
